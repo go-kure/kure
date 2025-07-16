125ea33cabc00dbef8caeee3084823b949e9748b
Serge van Ginderachter <serge@ginsys.eu>
2025-07-16 22:07:44 +0200

    Expand YAML type checking

--- internal/yaml/import_test.go
+++ internal/yaml/import_test.go
@@ -4,7 +4,13 @@ import (
 	"testing"
 
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 )
+
+type dummy struct{ runtime.TypeMeta }
+
+func (d *dummy) DeepCopyObject() runtime.Object { return &dummy{} }
 
 func TestParse(t *testing.T) {
 	data := `apiVersion: v1
@@ -28,5 +34,23 @@ spec:
 	}
 	if pod, ok := objs[1].(*corev1.Pod); !ok || pod.Name != "pod" {
 		t.Fatalf("unexpected second object: %#v", objs[1])
+	}
+}
+
+func TestCheckType(t *testing.T) {
+	pod := &corev1.Pod{}
+	pod.TypeMeta = metav1.TypeMeta{Kind: "Pod", APIVersion: "v1"}
+	if err := checkType(pod); err != nil {
+		t.Fatalf("expected pod to be supported: %v", err)
+	}
+
+	var unknown runtime.Object = &dummy{}
+	if err := checkType(unknown); err == nil {
+		t.Fatalf("expected unsupported object error")
+	}
+
+	bad := &corev1.Pod{TypeMeta: metav1.TypeMeta{Kind: "ServiceAccount", APIVersion: "v1"}}
+	if err := checkType(bad); err == nil {
+		t.Fatalf("expected type mismatch error")
 	}
 }
