c9e13da0eafb31ee320d4cab08eb61f7f9f7509e
Serge van Ginderachter <serge@ginsys.eu>
2025-07-22 18:16:23 +0200

    Refactor CLI with subcommands

--- cmd/kure/main.go
+++ cmd/kure/main.go
@@ -13,12 +13,17 @@ import (
 )
 
 func runCluster(args []string) error {
-	fs := flag.NewFlagSet("cluster", flag.ExitOnError)
+	fs := flag.NewFlagSet("cluster", flag.ContinueOnError)
+	fs.Usage = func() {
+		fmt.Fprintln(fs.Output(), "Usage of cluster:")
+		fs.PrintDefaults()
+	}
 	var configPath, manifestsPath, fluxPath string
 	fs.StringVar(&configPath, "config", "", "Path to cluster config YAML file")
 	fs.StringVar(&manifestsPath, "manifests", "manifests", "Output path for Kubernetes manifests")
 	fs.StringVar(&fluxPath, "flux", "flux", "Output path for FluxCD resources")
 	if err := fs.Parse(args); err != nil {
+		fs.Usage()
 		return err
 	}
 	if configPath == "" {
@@ -36,11 +41,16 @@ func runCluster(args []string) error {
 }
 
 func runPatch(args []string) error {
-	fs := flag.NewFlagSet("patch", flag.ExitOnError)
+	fs := flag.NewFlagSet("patch", flag.ContinueOnError)
+	fs.Usage = func() {
+		fmt.Fprintln(fs.Output(), "Usage of patch:")
+		fs.PrintDefaults()
+	}
 	var basePath, patchPath string
 	fs.StringVar(&basePath, "base", "", "Path to base YAML file")
 	fs.StringVar(&patchPath, "patch", "", "Path to patch file")
 	if err := fs.Parse(args); err != nil {
+		fs.Usage()
 		return err
 	}
 	if basePath == "" || patchPath == "" {
@@ -60,13 +70,25 @@ func runPatch(args []string) error {
 }
 
 func main() {
-	if len(os.Args) > 1 && os.Args[1] == "patch" {
-		if err := runPatch(os.Args[2:]); err != nil {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "expected 'cluster' or 'patch' subcommands")
+		os.Exit(1)
+	}
+
+	cmd := os.Args[1]
+	args := os.Args[2:]
+
+	switch cmd {
+	case "cluster":
+		if err := runCluster(args); err != nil {
+			log.Fatalf("cluster error: %v", err)
+		}
+	case "patch":
+		if err := runPatch(args); err != nil {
 			log.Fatalf("patch error: %v", err)
 		}
-		return
-	}
-	if err := runCluster(os.Args[1:]); err != nil {
-		log.Fatalf("cluster error: %v", err)
+	default:
+		fmt.Fprintln(os.Stderr, "expected 'cluster' or 'patch' subcommands")
+		os.Exit(1)
 	}
 }
