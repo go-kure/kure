48c97c8ada88d2367e639ba67c7e728bf094630c
Serge van Ginderachter <serge@ginsys.eu>
2025-07-23 17:41:42 +0200

    Add AppSet helper and label splitting

---
+++ pkg/cluster/add.go
@@ -0,0 +1,30 @@
+package cluster
+
+import (
+	"github.com/go-kure/kure/pkg/api"
+	"github.com/go-kure/kure/pkg/cluster/appset"
+	"github.com/go-kure/kure/pkg/layout"
+)
+
+// AddAppSet converts the provided AppSet into layout structures and appends
+// them to the manifest and Flux layout slices.
+func AddAppSet(manifests *[]*layout.ManifestLayout, fluxes *[]*layout.FluxLayout, as *appset.AppSet, filePer api.FileExportMode) error {
+	if err := as.Validate(); err != nil {
+		return err
+	}
+
+	ml := &layout.ManifestLayout{
+		Name:      as.Name,
+		Namespace: as.Namespace,
+		FilePer:   filePer,
+		Resources: as.Resources,
+	}
+	fl := &layout.FluxLayout{
+		Name:     as.Name,
+		Manifest: ml,
+	}
+
+	*manifests = append(*manifests, ml)
+	*fluxes = append(*fluxes, fl)
+	return nil
+}
